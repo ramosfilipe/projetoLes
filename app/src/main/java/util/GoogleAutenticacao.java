dda1f22ba424d2490f4c0264e6a1f1a59dd9e4c4
ramosfilipe <filipecadastros@me.com>
2014-06-17 23:10:50 -0300

    Build Preparado para o google play services

---
+++ app/src/main/java/util/GoogleAutenticacao.java
@@ -0,0 +1,11 @@
+package util;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+
+/**
+ * Criado por Filipe Ramos em 17/06/14 as 23.
+ */
+public class GoogleAutenticacao {
+    GoogleAuthUtil teste;
+
+}
